fee7edf8b406e19dc7241e020b036fa606a77b53
JuliaKuznetsovaQA <kuznetsovajulia@gmail.com>
2024-02-10 20:57:07 +0300

    add changes to file lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -58,12 +58,12 @@
 # print(film.keys())
 # print(film.values())
 # print(film.items())
+
 """3.6. Найдите сумму всех значений в словаре 
 my_dictionary = {'num1': 375, 'num2': 567, 'num3': -37, 'num4': 21}"""
 
 # my_dictionary = {'num1': 375, 'num2': 567, 'num3': -37, 'num4': 21}
 # print(sum(my_dictionary.values()))
-
 
 """3.7. Удалите повторяющиеся значения из списка [1, 2, 3, 4, 5, 3, 2, 1]"""
 
@@ -82,7 +82,9 @@ my_dictionary = {'num1': 375, 'num2': 567, 'num3': -37, 'num4': 21}"""
      - найдите значения, которые не встречаются в обоих множествах
      # - проверьте являются ли эти множества подмножествами друг друга"""
 
-# set1 = {'a', 'z', 1, 5, 9, 12, 100, 'b'}
-# set2 = {5, 'z', 1, 8, 9, 21, 100, 'l', 785}
-# print(set1 & set2)
-# print(set1 - (set1 & set2) | set2 - (set1 & set2))
+set1 = {'a', 'z', 1, 5, 9, 12, 100, 'b'}
+set2 = {5, 'z', 1, 8, 9, 21, 100, 'l', 785}
+print(set1 & set2)
+print(set1.intersection(set2))
+print(set1 - (set1 & set2) | set2 - (set1 & set2))
+print(set1.symmetric_difference(set2))
