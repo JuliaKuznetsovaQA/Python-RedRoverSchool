a1bebe925f44e6546339a2ebc25b3b1947ee1e91
JuliaKuznetsovaQA <kuznetsovajulia@gmail.com>
2024-03-07 23:03:59 +0300

    update file lesson6.py

--- lesson6.py
+++ lesson6.py
@@ -10,23 +10,10 @@
 
 file_name = 'words.txt'
 with open(file_name, 'r', encoding='utf-8') as file:
-    # result = set()
-    # for line in file:
-    #     word = file.readline().upper()
-    #     c = word[-2:]
-    #     if c == 'ЕЯ':
-    #         print(word)
-    #     else:
-    #         continue
-    # res = sorted(words)
-    # print(result)
     lst = set()
     for word in file:
-        a = word.strip().split()
-        for i in a:
-            c = i[-2:]
-            if c.upper() == 'ЕЯ':
-                lst.add(i.upper())
+        if word.upper().strip()[-2:] == 'ЕЯ':
+            lst.add(word.upper().strip())
     for i in sorted(lst, key=lambda x: (len(x), x)):
         print(i)
 
